8cab679fcda14d524f143fd907ddb82cf7a477a8
Ivy <erer1995@outlook.com>
2019-05-19 21:54:59 +1000

    add a shopping cart page

--- modeldb/cart.js
+++ modeldb/cart.js
@@ -5,11 +5,11 @@ module.exports = function Cart(oldCart) {
 
     this.add = (item,id) => {
         let storedItem = this.items[id];
-        if(!sotredItem) {
+        if(!storedItem) {
             storedItem = this.items[id] = {item: item, qty:0,price:0};
         }
         storedItem.qty++;
-        storedItem.price = stpredItem.item.price * storedItem.qty;
+        storedItem.price = storedItem.item.price * storedItem.qty;
         this.totalQty++;
         this.totalPrice += storedItem.item.price;
     };
@@ -17,7 +17,8 @@ module.exports = function Cart(oldCart) {
     this.generateArray = ()=>{
         var arr = [];
         for(let id in this.items){
-            err.push(this.items[id]);
+            arr.push(this.items[id]);
+            console.log(this.items[id].item.title)
         }
         return arr;
     }
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Cart = require('../modeldb/cart');
 
 //bring in users model
 let Menu = require('../modeldb/items');
@@ -24,14 +25,21 @@ router.get('/cart/:id',function(req,res){
     let cart = new Cart(req.session.cart ? req.session.cart :{items:{}})
      Menu.findById(pID, (err,item) => {
          if(err){
-             console.log('Hello!')
              return res.redirect('/')
          } 
          cart.add(item,item.id);
          req.session.cart = cart;
-         console.log(req.session.cart);
-         res.redirect('/')
+         //console.log(req.session.cart);
+         res.redirect('/');
      });
  });
+
+ router.get('/add_to_cart', (req,res,next) =>{
+     if(!req.session.cart){
+         return res.render('add_to_cart',{items: null})
+     }
+     var cart = new Cart(req.session.cart);
+     res.render('add_to_cart',{items: cart.generateArray(),totalPrice:cart.totalPrice,totalQty: cart.totalQty})
+ })
 
 module.exports = router;
